ac27e95f1cecb3e440ad7e58af96bca02abfcbd4
Gravender <ss.noah@gmail.com>
2021-02-10 21:24:41 +1100

    Made a working Home Page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import { loadMenu } from './modules/menu'
 import { loadContact } from './modules/contact'
 import { loadFooter } from './modules/footer';
 
-
+const header =  () => {
+    const content = document.getElementById('content');
+    const head = document.createElement("header");
+    head.innerHTML =`
+    <nav>
+        <h2>Good Burger</h2>
+        <ul class ="tabs">
+            <li data-target = '#home' class = 'tab active'>Home</li>
+            <li data-target = '#menu' class = 'tab'>Menu</li>
+            <li data-target = '#contact' class = 'tab'>Contact</li>
+        </ul>
+    </nav>
+    `;
+    content.appendChild(head);
+};
+header();
 loadHome();
 loadFooter();
